dd6318d6e8fbafc0a450177ac1642af03170082c
Ali Ghofrani <alighofrani1383@gmail.com>
2026-06-15 15:00:15 +0330

    first commit

---
+++ .history/script_20260615143046.js
@@ -0,0 +1,44 @@
+// -- Global --
+const bookmarksBtnEl = document.querySelector('.bookmarks-btn');
+const errorEl = document.querySelector('.error');
+const errorTextEl = document.querySelector('.error__text');
+const jobDetailsEl = document.querySelector('.job-details');
+const jobDetailsContentEl = document.querySelector(".job-details__content");
+const jobListBookmarksEl = document.querySelector('.job-list--bookmarks');
+const jobListSearchEl = document.querySelector(".job-list--search");
+const numberEl = document.querySelector(".count__number");
+const paginationEl = document.querySelector(".pagination");
+const paginationBtnNextEl = document.querySelector(".pagination__button--next");
+const paginationBtnBackEl = document.querySelector(".pagination__button--back");
+const paginationNumberNextEl = document.querySelector(".pagination__number--next");
+const paginationNumberBackEl = document.querySelector(".pagination__number--back");
+const searchFormEl = document.querySelector(".search");
+const searchInputEl = document.querySelector(".search__input");
+const sortingEl = document.querySelector(".sorting");
+const sortingBtnRelevantEl = document.querySelector(".sorting__button--relevant");
+const sortingBtnRecentEl = document.querySelector(".sorting__button--recent");
+const spinnerSearchEl = document.querySelector(".spinner--search");
+const spinnerJobDetailsEl = document.querySelector(".spinner--job-details");
+
+// SEARCH COMPONENT
+const submitHandler = event => {
+    event.preventDefault();
+
+    //get input search text
+    const searchText = searchInputEl.value;
+
+    //Validation
+    const forbiddenPattern = /[0-9]/;
+    const patternMatch = forbiddenPattern.test(searchText);
+    if (patternMatch) {
+        errorTextEl.textContent = "your search may not contain number";
+        errorEl.classList.add('error--visible');
+        setTimeout(() => {
+            errorEl.classList.remove('error--visible');
+        }, 3000);
+    }
+searchInputEl.blur();
+spinnerSearchEl.classList.add('spinner--visible');
+spinnerSearchEl.classList.add('spinner--visible');
+};
+searchFormEl.addEventListener('submit', submitHandler);
